7ca3ddc5912211c4eaa2cd8080d02908b11aed1d
Lars Sjöström <lars@radicore.se>
2024-09-17 22:22:39 +0200

    chore: nix flake fmt

--- flake.nix
+++ flake.nix
@@ -6,36 +6,36 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }: 
+  outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      devPkgs = with pkgs; [
-        golangci-lint
-        buf
-        delve
-        git
-        go_1_23
-        go-task
-        golines
-        goreleaser
-        gotestsum
-        glibcLocales
-        ko
-        protobuf
-        protoc-gen-go
-        watchexec
-      ];
-    in 
-    {
-      devShells = {
-        default = pkgs.mkShell {
-          LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-          packages = devPkgs;
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-      };
-    }
-  );
+        devPkgs = with pkgs; [
+          golangci-lint
+          buf
+          delve
+          git
+          go_1_23
+          go-task
+          golines
+          goreleaser
+          gotestsum
+          glibcLocales
+          ko
+          protobuf
+          protoc-gen-go
+          watchexec
+        ];
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+            packages = devPkgs;
+          };
+        };
+      }
+    );
 }
