96dbad085c2310d125946655aaf29cc05e410f02
Lars Sjöström <lars@radicore.se>
2024-10-01 15:57:28 +0200

    chore: remove test that relay on fetching well-known from google idp

--- oidc/oidc_test.go
+++ oidc/oidc_test.go
@@ -10,12 +10,4 @@ import (
 func TestOIDCProvider(t *testing.T) {
 	_, err := NewOIDCProvider("", "", "", "", nil)
 	assert.Error(t, err)
-
-	// test google provider
-	p, err := NewOIDCProvider("client_id", "client_secret", "http://localhost:8080", "https://accounts.google.com", []string{"email", "profile"})
-	assert.NotNil(t, p)
-	assert.Nil(t, err)
-
-	url := p.IdpAuthURL("code_challenge123")
-	assert.NotEmpty(t, url)
 }
