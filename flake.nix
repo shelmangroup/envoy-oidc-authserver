6cbd91eb1017dbfa735e891b01c2dd92e1f7d9dc
Daniel Lundin <dln@arity.se>
2024-10-01 15:31:16 +0200

    nix: add container image (#3)
    * nix: add package
    
    * nix: add container image
    
    ---------
    
    Co-authored-by: Lars Sjöström <lars@radicore.se>

--- flake.nix
+++ flake.nix
@@ -15,6 +15,8 @@
     utils.lib.eachDefaultSystem (
       system:
       let
+        inherit (nixpkgs) lib;
+
         pkgs = import nixpkgs {
           inherit system;
         };
@@ -36,22 +38,37 @@
         ];
       in
       {
-        packages.default = pkgs.buildGo123Module {
-          pname = "envoy-oidc-authserver";
-          version = "main";
-          src = ./.;
+        packages = {
+          default = pkgs.buildGo123Module {
+            pname = "envoy-oidc-authserver";
+            version = "main";
+            src = ./.;
 
-          # vendorHash = ""; # Use this when upgrading dependencies
-          vendorHash = "sha256-n7+O+uc8YRnhXccVVnKE6zK8kh8EGiKeiES4A+R1Dhg=";
+            meta = {
+              desciption = "Envoy OIDC Authserver";
+              homepage = "https://github.com/shelmangroup/envoy-oidc-authserver";
+              mainProgram = "envoy-oidc-authserver";
+            };
 
-          nativeBuildInputs = with pkgs; [
-            buf
-            protoc-gen-go
-          ];
+            # vendorHash = ""; # Use this when upgrading dependencies
+            vendorHash = "sha256-n7+O+uc8YRnhXccVVnKE6zK8kh8EGiKeiES4A+R1Dhg=";
 
-          prePatch = ''
-            HOME="$TMPDIR" ${pkgs.buf}/bin/buf generate
-          '';
+            nativeBuildInputs = with pkgs; [
+              buf
+              protoc-gen-go
+            ];
+
+            prePatch = ''
+              HOME="$TMPDIR" ${pkgs.buf}/bin/buf generate
+            '';
+          };
+
+          container = pkgs.dockerTools.buildImage {
+            name = "envoy-oidc-authserver";
+            tag = "latest";
+            copyToRoot = [ self.packages.${system}.default ];
+            config.Entrypoint = [ (lib.getExe self.packages.${system}.default) ];
+          };
         };
 
         devShells = {
